0fe352767a1b0e3d2394f686996e43a7ed11d312
Kushal-kothari <kushalkothari285@gmail.com>
2020-12-27 12:44:35 +0530

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -106,7 +106,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-HVJP80RNX0",
+        trackingId: "UA-186061894-1",
         head:true,
         anonymize:true,
       },
